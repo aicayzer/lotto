8a1a0d67cc03025fcfb9aced944a9c7b2a72309f
August Cayzer <august.cayzer@gousto.co.uk>
2020-03-02 11:25:16 +0000

    Updates

---
+++ precache-manifest.14e4e73821c593efaebd61e3c3ae3b58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36c02e875202500d5dd5fa8288202e92",
+    "url": "/lotto/index.html"
+  },
+  {
+    "revision": "2098d085cff134a2cbca",
+    "url": "/lotto/static/css/main.8a466585.chunk.css"
+  },
+  {
+    "revision": "5584b9b35314f4ab777e",
+    "url": "/lotto/static/js/2.348619c5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/lotto/static/js/2.348619c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2098d085cff134a2cbca",
+    "url": "/lotto/static/js/main.e0ae8ea7.chunk.js"
+  },
+  {
+    "revision": "7ac3918fa28ed4c56cdf",
+    "url": "/lotto/static/js/runtime-main.a2aad5eb.js"
+  }
+]);
